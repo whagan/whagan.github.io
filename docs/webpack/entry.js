fd396f1668de4e5044345a7012578f9a15050e24
Will Hagan <william_hagan@hotmail.com>
2021-06-25 17:06:55 -0500

    hello.js

--- docs/webpack/entry.js
+++ docs/webpack/entry.js
@@ -1,1 +1,11 @@
-console.log("Hello Will!")
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+import Hello from './components/Hello';
+class App extends Component {
+ render() {
+ return (
+ <Hello />
+ )
+ }
+}
+render(<App />, document.getElementById('root'));
